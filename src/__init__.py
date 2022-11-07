cf0269941b9c1051a20889059e5441f8328d3e00
Tom Kurowski <t.j.kurowski@cranfield.ac.uk>
2022-11-07 16:48:47 +0000

    Add __all__ to src __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,11 @@
 """Re-export module classes and functions."""
+
+__all__ = [
+    'GbsVcfReader', 'JmLocWriter',
+    'is_potential_marker', 'site_to_marker',
+    'natural_sort_key'
+]
+
 from .gbsvcfreader import GbsVcfReader
 from .jmlocwriter import JmLocWriter
 from .site_to_marker import is_potential_marker, site_to_marker
